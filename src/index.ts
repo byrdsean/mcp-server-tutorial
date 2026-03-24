993a718ee39fa5c1f88a51db857d2eb5f87514c0
Sean Byrd <byrd.sean1015@gmail.com>
2026-03-23 22:17:08 -0400

    Setting up a blank MCP server for learning purposes.

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import z from "zod";
+
+const main = async () => {
+    const mcpServer = new McpServer(
+        {
+            version: "1.0.0",
+            name: "MCP Tutorial",
+            description: "Tutorial to lear the basics of writing a MCP server for AI agents",
+            title: "MCP Tutorial"
+        }
+    )
+
+    const transport = new StdioServerTransport();
+    await mcpServer.connect(transport);
+    console.error("MCP Tutorial is running");
+}
+
+main()
