afe908aa75e34f840e443c7801c472a3329c603a
Sean Byrd <byrd.sean1015@gmail.com>
2026-03-25 23:35:12 -0400

    Adding resource and resource template.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { UriTemplate } from "@modelcontextprotocol/sdk/shared/uriTemplate.js";
 import z from "zod";
 
 const main = async () => {
@@ -41,6 +42,99 @@ const main = async () => {
                     {
                         type: "text",
                         text: tokuName
+                    }
+                ]
+            }
+        }
+    );
+
+    mcpServer.registerResource(
+        "travel_all_activities",
+        "travel://activitives",
+        {
+            title: "Travel Activiites",
+            description: "Return a list of possible activities a traveler can do in all cities",
+            mimeType: "application/json"
+        },
+        async (uri) => {
+            console.error(`travel_activities - URI: ${uri}`);
+
+            const sanFran = {
+                city: "San Francisco",
+                activities: ["hiking", "snowboarding", "wine tasting"]
+            };
+
+            const newYork = {
+                city: "New York",
+                activities: ["bus tours", "sightseeing", "Statue of Liberty", "Central Park", "Hudson River"]
+            }
+
+            const activities = {
+                activities: [sanFran, newYork]
+            }
+
+            return {
+                contents: [
+                    {
+                        uri: uri.href,
+                        mimeType: "application/json",
+                        text: JSON.stringify(activities)
+                    }
+                ]
+            }
+        }
+    );
+
+    mcpServer.registerResource(
+        "travel_activities_by_city",
+        new ResourceTemplate(
+            "travel://activitives/city/{city}",
+            { list: undefined }
+        ),
+        {
+            title: "Traval Activiites By City",
+            description: "Given the name of a city, return a list of possible activities a traveler can do in that city",
+            mimeType: "application/json"
+        },
+        async (uri, { city }) => {
+            console.error(`travel_activities_by_city - URI: ${uri}, City: ${city}`);
+
+            const sanFran = {
+                city: "San Francisco",
+                activities: ["hiking", "snowboarding", "wine tasting"]
+            };
+
+            const newYork = {
+                city: "New York",
+                activities: ["bus tours", "sightseeing", "Statue of Liberty", "Central Park", "Hudson River"]
+            }
+
+            const activities = [sanFran, newYork];
+
+
+            let parsedCity= city?.toString();
+            if(parsedCity) {
+                parsedCity = decodeURIComponent(parsedCity);
+            } else {
+                parsedCity = "";
+            }
+
+            const activity = activities
+                .find(a => a.city.toLocaleLowerCase() === parsedCity.toLocaleLowerCase());
+
+            let text = "";
+            if (!activity) {
+                text = `Unable to find activities for city ${city}`;
+            } else {
+                text = JSON.stringify(activity);
+            }
+
+            return {
+                contents: [
+                    {
+                        uri: uri.href,
+                        mimeType: "application/json",
+                        text: text
                     }
                 ]
             }
